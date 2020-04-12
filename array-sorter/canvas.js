2628d13cf86855a01037d6218fdcd323306e0938
ZenNukesEarth <kenneth.suarez@upd.edu.ph>
2020-04-13 05:09:19 +0800

    animation working

--- array-sorter/canvas.js
+++ array-sorter/canvas.js
@@ -1,95 +1,163 @@
-window.addEventListener('load', () => {
-	const canvas = document.querySelector("#canvas");
-	const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+// resize canvas
+canvas.height = window.innerHeight / 2;
+canvas.width = window.innerWidth;
 
-	// resizing
-	canvas.height = window.innerHeight / 2;
-	canvas.width = window.innerWidth;
+// main variables used in program
+var aa = null;
+var currentlySorting = false;
+var arr = [];
+var DEFAULT_COLOR = "#b3f2ff";
+var COMPARE_COLOR = "red";
+var SWAP_COLOR = "lime";
 
-	function randInt(min,max) {
-		return min + Math.floor((max - min) * Math.random());
+function randInt(min,max) {
+	return min + Math.floor((max - min) * Math.random());
+} 
+
+// draw
+function draw(arr) {
+	//clear canvas before
+	ctx.clearRect(0, 0, canvas.width, canvas.height); 
+	var currX = 50;
+	var width = 30;
+	var arr_size = arr.length;
+	for (var i = 0; i < arr_size; i++) {
+		height = arr[i].height;
+		ctx.fillStyle = arr[i].color;
+		ctx.fillRect(currX, canvas.height - height, width, height);
+		currX += width + 10;
+	}
+}
+
+// generate array from slider input
+var slider = document.getElementById("arraySize");
+
+	// display array from initial value
+	for (var i = 0; i < slider.value; i++) {
+		arr.push({
+			height: randInt(10,200),
+			color: "#b3f2ff"
+		});
+	}
+	draw(arr);
+
+// modify array based using slider
+slider.oninput = function() {
+	if (currentlySorting) {
+		return;
 	} 
+	// generate new array
+	arr = [];
+	for (let i = 0; i < this.value; i++) {
+		arr.push({
+			height: randInt(10,200),
+			color: DEFAULT_COLOR
+		});
+	}
+ 	draw(arr);
+}
 
-	// draw
-	function draw(arr) {
-		//clear canvas before
-		ctx.clearRect(0, 0, canvas.width, canvas.height); 
-		var currX = 50;
-		var width = 30;
-		var arr_size = arr.length;
-		for (var i = 0; i < arr_size; i++) {
-			height = arr[i].height;
-			ctx.fillStyle = arr[i].color;
-			ctx.fillRect(currX, canvas.height - height, width, height);
-			currX += width + 10;
-		}
+// function for visualizing array 
+function Animate(arr) {
+	this._arr = arr;
+	this.displayArray = []
+	this.actions = [];
+
+	for(var i = 0; i < arr.length; i++) {
+		this.displayArray.push(arr[i]);
+	}
+	var _this = this;
+	this.id = setInterval(() => {
+		_this.step();
+	}, 100);
+}
+Animate.prototype.step = function() {
+	if (this.actions.length == 0) {
+		draw(this._arr); 
+		return;
 	}
 
-	// generate array from slider input
-	var slider = document.getElementById("arraySize");
-	var arr = [];
-		// display array from initial value
-		for (var i = 0; i < slider.value; i++) {
-			arr.push({
-				height: randInt(10,200),
-				color: "#b3f2ff"
-			});
-		}
-		draw(arr);
-
-	// animation
-	function AnimatedArray(arr, canvas) {
-		this._arr = arr;
-		this._canvas = canvas;
-		var arr_size = arr.length;
-		var _this = this;
-		this._id = setInterval(function() {_this._step();}, 1000);
-
+	var action = this.actions.shift();
+	var i = action[1];
+	var j = action[2];
+	if (action[0] == 'compare') {
+		this.displayArray[i].color = COMPARE_COLOR;
+		this.displayArray[j].color = COMPARE_COLOR;
 	}
-
-	AnimatedArray.prototype._step = function() {
-		this._arr = [];
-		for (var i = 0; i < 10; i++) {
-			this._arr.push({
-				height: randInt(10,200),
-				color: "#b3f2ff"
-			});
-		}
-		draw(this._arr);
-	};
-	// modify array based using slider
-	slider.oninput = function() {
-		// generate new array
-		arr = [];
-		for (let i = 0; i < this.value; i++) {
-			arr.push({
-				height: randInt(10,200),
-				color: "#b3f2ff"
-			});
-		}
-		// bubbleSort(arr);
-		// display on canvas
-		var aa = new AnimatedArray(arr, canvas);
-		bubbleSort(aa);
-	 	//draw(arr);
+	if (action[0] == 'swap') {
+		this.displayArray[i].color = SWAP_COLOR;
+		this.displayArray[j].color = SWAP_COLOR;
+		var temp = this.displayArray[i];
+		this.displayArray[i] = this.displayArray[i+1];
+		this.displayArray[i+1] = temp;
 	}
+	// draw array
+	draw(this.displayArray);
+	// revert color after changes
+	this.displayArray[i].color = DEFAULT_COLOR;
+	this.displayArray[j].color = DEFAULT_COLOR;
 
-});
+}
 
+Animate.prototype.greaterThan = function(i, j) {
+	this.actions.push(['compare', i, j]);
+	// show values currently being compared
+	return this._arr[i].height > this._arr[j].height;
+}
 
-function bubbleSort(aa) {
-	let n = arr.length;
+Animate.prototype.swap = function(i, j) {
+	this.actions.push(['swap', i, j]);
+	// array
+	var temp = this._arr[i];
+	this._arr[i] = this._arr[i+1];
+	this._arr[i+1] = temp;
+}
+
+Animate.prototype.cancel = function() {
+	clearInterval(this.id);
+}
+// event when start button is clicked
+var visualArr = null;
+function BeginSort() {
+	// create an instance of visualizer
+	visualArr = new Animate(arr);
+	// delay in animation will allow program to 
+	// process the steps necessary in sorting
+	insertionSort(visualArr);
+}
+
+// event when stop button is clicked
+function StopSort() {
+	if (visualArr != null) {
+		visualArr.cancel();
+	}
+	currentlySorting = false;
+}
+
+// the following are sorting algorithms 
+// running the sorting algorithms will
+// generate the step by step needed
+function bubbleSort(visualArr) {
+	let n = visualArr._arr.length;
 	for (let i = 0; i < n; i++) {
 		for (let j = 0; j < n - i - 1; j++) {
-				if (arr[j+1].height < arr[j].height) {
-				var swap = arr[j].height;
-				arr[j].height = arr[j+1].height;
-					arr[j+1].height = swap;
+			if (visualArr.greaterThan(j, j+1)) {
+				visualArr.swap(j, j+1);
 			}
 		}
 	}	
 }
+// 0 1 2 3 4 5
 
-function beginSort() {
-	
+function insertionSort(visualArr) {
+	var n = visualArr._arr.length;
+	for (var i = 0; i < n; i++) {
+		var j = i;
+		while (j > 0 && visualArr.greaterThan(j-1, j)) {
+			visualArr.swap(j-1,j);
+			j -= 1;
+		}
+	}
 }
